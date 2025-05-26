e18fc88b368fd2576624311ab55d61878ed0ca80
Bibash <bibash8@gmail.com>
2025-05-26 13:08:13 +0545

    Initial Progress Started

---
+++ JavaProject/src/javaproject/database/MySqlConnection.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaproject.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ACER
+ */
+public class MySqlConnection implements DbConnection {
+    public Connection openConnection() {
+       String username="root";
+       String password="Your MySql password";
+       String database="marks";
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection conn;
+           conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/"+database,username,password);
+           return conn;
+       }
+       catch(Exception e){
+          return null;
+       }
+    }
+
+    public void closeConnection(Connection conn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public ResultSet runQuery(Connection conn, String query) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public int executeUpdate(Connection conn, String query) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
